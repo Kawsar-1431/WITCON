63d95994fa3de6e6b5b70e965f2e44512df7a94c
Kawsar Alam khan <kawsarjahin@gmail.com>
2025-03-24 22:22:10 +0000

    Final

--- models/flightModel.js
+++ models/flightModel.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose');
 
 const flightSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please enter the name of the flight"]
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter the name of the flight"]
+        },
+        seats: {
+            type: Number,
+            required: [true, "Please enter the number of seats"]
+        },
+        price: {
+            type: Number,
+            required: [true, "Please enter the price of the flight ticket"]
+        },
+        origin: {
+            type: String,
+            required: true
+        },
+        destination: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,  // Store as a Date type
+            required: true
+        }
     },
-    seats: {
-      type: Number,
-      required: [true, "Please enter the number of seats"]
-    },
-    price: {
-      type: Number,
-      required: [true, "Please enter the price of the flight ticket"]
-    },
-    origin: {
-      type: String,
-      required: true
-    },
-    destination: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: Date,  // Store as a Date type
-      required: true
+    {
+        timestamps: true
     }
-  },
-  {
-    timestamps: true
-  }
 );
 
 const Flight = mongoose.model('Flight', flightSchema);
